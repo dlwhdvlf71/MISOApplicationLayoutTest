c7cc24d2568b525e170fb629b70c39ed8338e65b
Anony <41979552+dlwhdvlf71@users.noreply.github.com>
2023-03-19 22:03:58 +0900

    초기 커밋.

---
+++ MauiMISOTest/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiMISOTest;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiMISOTest/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiMISOTest;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
